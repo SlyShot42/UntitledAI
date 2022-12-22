cf20f6603bdfaecacf49404189b4d552182e5adf
Viraj <virajmohit7@gmail.com>
2022-12-21 18:17:47 -0800

    added some lines to start screen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Hello world. I am alive now.</Text>
       <StatusBar style="auto" />
     </View>
   );
