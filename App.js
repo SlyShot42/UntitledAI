5ffbbed478aa6f648c072d731599cc574db42006
Mark Ortega <TheGoat@Marilus-MacBook-Pro.local>
2022-12-26 17:14:37 -0800

    Testing mark 12

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world. I am dead now.</Text>
+      <Text>Hello world. I am never dead.</Text>
       <StatusBar style="auto" />
     </View>
   );
