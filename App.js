3f80c6f09d47bb23b54647e09f67b29ad31724b7
Viraj <virajmohit7@gmail.com>
2022-12-27 16:35:26 -0800

    added basic ui elements

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SearchBar } from 'react-native-ios-kit';
+import { PageControlView } from 'react-native-ios-kit';
+import { Title1 } from 'react-native-ios-kit';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world. I am killed now.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello world. I am killed now.</Text>
+        <View style={{ flex: 0.7 }}>
+          <PageControlView defaultPage={1}>
+            <View style={styles.container}>
+              <Title1>First page.</Title1>
+            </View>
+            <View style={styles.container}>
+              <Title1>Second page.</Title1>
+            </View>
+            <View style={styles.container}>
+              <Title1>Third page.</Title1>
+            </View>
+          </PageControlView>
+        </View>
+        <View style={{ flex: 0.3 }}>
+          <SearchBar
+            value={this.state.text}
+            onValueChange={text => this.setState({ text })}
+            withCancel
+            animated
+          />
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +52,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
+
+
+
