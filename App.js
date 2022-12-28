9f694979b837a4f81f6d65bd735752822d6a95ca
Mark Ortega <TheGoat@Marilus-MacBook-Pro.local>
2022-12-27 16:36:30 -0800

    Testing mark 12

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+
+  const askName = () => {
+    Alert.prompt(
+      'What is your name?',
+      'Enter your name below:',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: (name) => setName(name),
+        },
+      ],
+      'plain-text'
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello world. I am never dead.</Text>
+      {name ? (
+        <Text>Hello {name}!</Text>
+      ) : (
+        <Text onPress={askName}>
+          Hello world. Click here to tell me your name.
+        </Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
