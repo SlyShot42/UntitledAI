fa956a83794c551b332ef87fcaf32af51e0e331e
Viraj <virajmohit7@gmail.com>
2022-12-29 14:13:43 -0800

    new feature: page creation

--- App.js
+++ App.js
@@ -1,45 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { SearchBar } from 'react-native-ios-kit';
 import { PageControlView } from 'react-native-ios-kit';
 import { Title1 } from 'react-native-ios-kit';
+import { DefaultTheme, ThemeProvider } from 'react-native-ios-kit';
+import color from 'color';
+import { KeyboardAvoidingView } from 'react-native';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       text: '',
+      pages: [
+        {
+          title: 'First page',
+          content: 'This is the content for the first page',
+        },
+        {
+          title: 'Second page',
+          content: 'This is the content for the second page',
+        },
+        {
+          title: 'Third page',
+          content: 'This is the content for the third page',
+        },
+      ],
     };
   }
+  
+addPage = text => {
+  this.setState(prevState => ({
+    pages: [
+      ...prevState.pages,
+      {
+        title: text,
+        content: 'This is the content for the new page',
+      },
+    ],
+  }));
+};
+
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello world. I am killed now.</Text>
-        <View style={{ flex: 0.7 }}>
-          <PageControlView defaultPage={1}>
-            <View style={styles.container}>
-              <Title1>First page.</Title1>
-            </View>
-            <View style={styles.container}>
-              <Title1>Second page.</Title1>
-            </View>
-            <View style={styles.container}>
-              <Title1>Third page.</Title1>
-            </View>
-          </PageControlView>
-        </View>
-        <View style={{ flex: 0.3 }}>
-          <SearchBar
-            value={this.state.text}
-            onValueChange={text => this.setState({ text })}
-            withCancel
-            animated
-          />
-        </View>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+          <View style={{ flex: 0.92 }}>
+            <PageControlView defaultPage={1}>
+              {this.state.pages.map(page => (
+                <View style={styles.container}>
+                  <Title1>{page.title}</Title1>
+                  <Text>{page.content}</Text>
+                </View>
+              ))}
+            </PageControlView>
+          </View>
+          <View style={{ flex: 0.08 }}>
+            <SearchBar
+              value={this.state.text}
+              onValueChange={this.addPage}
+              withCancel
+              animated={true}
+            />
+          </View>
+          <StatusBar style="auto" />
+        </KeyboardAvoidingView>
+      </SafeAreaView> 
     );
   }
 }
@@ -54,6 +82,7 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
 
 
 
