de2bf2550ae497cba483aa33dc62aee50e57464f
Mark Ortega <TheGoat@marilus-mbp.lan>
2022-12-29 00:29:06 -0800

    blank slate

--- App.js
+++ App.js
@@ -1,59 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SearchBar } from 'react-native-ios-kit';
-import { PageControlView } from 'react-native-ios-kit';
-import { Title1 } from 'react-native-ios-kit';
+import React from 'react';
+import { Button, View, Text } from 'react-native';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '',
-    };
-  }
+export default function App() {
+  const [color, setColor] = React.useState('white');
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello world. I am killed now.</Text>
-        <View style={{ flex: 0.7 }}>
-          <PageControlView defaultPage={1}>
-            <View style={styles.container}>
-              <Title1>First page.</Title1>
-            </View>
-            <View style={styles.container}>
-              <Title1>Second page.</Title1>
-            </View>
-            <View style={styles.container}>
-              <Title1>Third page.</Title1>
-            </View>
-          </PageControlView>
-        </View>
-        <View style={{ flex: 0.3 }}>
-          <SearchBar
-            value={this.state.text}
-            onValueChange={text => this.setState({ text })}
-            withCancel
-            animated
-          />
-        </View>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: color }}>
+      <Button
+        onPress={() => setColor('red')}
+        title="Change color to red"
+      />
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default App;
-
-
-
