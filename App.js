0531a73da55004250d2f286ca1140ad9db2302bc
Viraj <virajmohit7@gmail.com>
2022-12-30 16:11:22 -0800

    updated design elements

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { Title1 } from 'react-native-ios-kit';
 import { DefaultTheme, ThemeProvider } from 'react-native-ios-kit';
 import color from 'color';
 import { KeyboardAvoidingView } from 'react-native';
+import { Image } from 'react-native'
 
 
 class App extends Component {
@@ -28,19 +29,27 @@ class App extends Component {
           content: 'This is the content for the new page',
         },
       ],
-      currentPage: 2, // Set the current page to the last element
+      currentPage: 2 // Set the current page to the last element
     }));
     this.setState({ text: '' })
   };
   
 
+  
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
+        
         <KeyboardAvoidingView style={styles.container} behavior="padding">
-          <View style={{ flex: 0.92 }}>
-            <PageControlView defaultPage={2}>
+          <View style={{ flex: 0.1, justifyContent: 'center', alignItems: 'center' }}>
+            <Image
+              source={{ uri: 'https://static.wixstatic.com/media/55a99a_cc804f3d039b44f9be6a15c5f1ae8550~mv2.png/v1/fill/w_1024,h_1024,al_c,q_90,enc_auto/My%20project-1.png' }}
+              style={{ width: 100, height: 100, marginTop: 0, }}
+            />
+          </View>
+          <View style={{ flex: 0.82 }}>
+            <PageControlView defaultPage={2} currentPageIndicatorTintColor='red'>
               {this.state.pages.map(page => (
                 <View style={styles.container}>
                   <Title1>{page.title}</Title1>
