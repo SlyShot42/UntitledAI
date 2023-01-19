05289bbd05c9026508fb21baad90a5e880bb7550
Viraj <virajmohit7@gmail.com>
2023-01-19 01:22:26 -0800

    removed carousel for the time being and added features to prompt bar

--- App.js
+++ App.js
@@ -1,92 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { SearchBar } from 'react-native-ios-kit';
-import { PageControlView } from 'react-native-ios-kit';
-import { Title1 } from 'react-native-ios-kit';
-import { DefaultTheme, ThemeProvider } from 'react-native-ios-kit';
-import color from 'color';
+import { Ionicons } from "@expo/vector-icons"
+import { TouchableOpacity } from 'react-native'
+import React, { Component, useState } from 'react';
+import { ScrollView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { View } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native';
-import { Image } from 'react-native'
+import { TextInput } from 'react-native';
 
+export default function App() {
+  const [input, setInput] = useState('');
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '',
-      pages: [],
-      currentPage: 0,
-    };
-  }
-  
-  addPage = () => {
-    this.setState(prevState => ({
-      pages: [
-        ...prevState.pages,
-        {
-          title: this.state.text,
-          content: 'This is the content for the new page',
-        },
-      ],
-      currentPage: 2 // Set the current page to the last element
-    }));
-    this.setState({ text: '' })
-  };
-  
-
-  
-
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
-          <View style={{ flex: 0.1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image
-              source={{ uri: 'https://static.wixstatic.com/media/55a99a_cc804f3d039b44f9be6a15c5f1ae8550~mv2.png/v1/fill/w_1024,h_1024,al_c,q_90,enc_auto/My%20project-1.png' }}
-              style={{ width: 100, height: 100, marginTop: 0, }}
+  const submitPrompt = () => {};
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+        keyboardVerticalOffset={0}
+        >
+          
+          <ScrollView></ScrollView>
+          <View style={styles.footer}>
+            <TextInput
+            allowFontScaling
+            style={styles.input}
+            placeholder='enter prompt...'
+            placeholderTextColor='grey'
+            value={input}
+            onChange={text => setInput(text)}
             />
+            <TouchableOpacity onPress={submitPrompt} activeOpacity={0.5}>
+              <Ionicons name='arrow-forward-circle-outline' size={30} color='#2B68E6'/>
+            </TouchableOpacity>
           </View>
-          <View style={{ flex: 0.82 }}>
-            <PageControlView defaultPage={2} currentPageIndicatorTintColor='red'>
-              {this.state.pages.map(page => (
-                <View style={styles.container}>
-                  <Title1>{page.title}</Title1>
-                  <Text>{page.content}</Text>
-                </View>
-              ))}
-            </PageControlView>
-          </View>
-          <View style={{ flex: 0.08 }}>
-            <SearchBar
-              value={this.state.text}
-              onValueChange={text => this.setState({ text })}
-              onBlur={this.addPage}
-              withCancel
-              animated={true}
-            />
-                     </View>
-          <StatusBar style="auto" />
-        </KeyboardAvoidingView>
-      </SafeAreaView> 
-    );
-  }
+       
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  footer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    padding: 10
+  },
+  input: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 10,
+    borderColor: 'transparent',
+    backgroundColor: '#ECECEC',
+    borderWidth: 1,
+    padding: 10,
+    color: 'grey',
+    borderRadius: 30,
   },
 });
-
-export default App;
-
-
-
-
-
-
