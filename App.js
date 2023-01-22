4afbb869aa3c7685b7474f1e458e4fec4577042a
Viraj <virajmohit7@gmail.com>
2023-01-21 19:58:08 -0800

    updated styles on the the search bar

--- App.js
+++ App.js
@@ -7,34 +7,61 @@ import { StyleSheet, SafeAreaView } from 'react-native';
 import { View } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native';
 import { TextInput } from 'react-native';
+import { Keyboard } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native';
+import Carousel from 'react-native-snap-carousel'
+import * as Haptics from 'expo-haptics';
+
 
 export default function App() {
   const [input, setInput] = useState('');
 
-  const submitPrompt = () => {};
+
+  const submitPrompt = () => {
+    Keyboard.dismiss()
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+  };
+
+  const _renderItem = ({item, index}) => {
+    return (
+        <View style={styles.slide}>
+            <Text style={styles.title}>{ item.title }</Text>
+        </View>
+    );
+  };
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <KeyboardAvoidingView 
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.container}
-        keyboardVerticalOffset={0}
+        keyboardVerticalOffset={0.5}
         >
-          
-          <ScrollView></ScrollView>
-          <View style={styles.footer}>
-            <TextInput
-            allowFontScaling
-            style={styles.input}
-            placeholder='enter prompt...'
-            placeholderTextColor='grey'
-            value={input}
-            onChange={text => setInput(text)}
-            />
-            <TouchableOpacity onPress={submitPrompt} activeOpacity={0.5}>
-              <Ionicons name='arrow-forward-circle-outline' size={30} color='#2B68E6'/>
-            </TouchableOpacity>
-          </View>
-       
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss()}>
+            <>
+              {/* <Carousel
+                ref={(c) => { this._carousel = c; }}
+                data={this.state.entries}
+                renderItem={_renderItem}
+                sliderWidth={sliderWidth}
+                itemWidth={itemWidth}
+              /> */}
+              <ScrollView></ScrollView>
+              <View style={styles.footer}>
+                <TextInput
+                allowFontScaling
+                style={styles.input}
+                placeholder='enter prompt...'
+                placeholderTextColor='grey'
+                value={input}
+                onChange={text => setInput(text)}
+                />
+                <TouchableOpacity onPress={submitPrompt} activeOpacity={0.5}>
+                  <Ionicons name='arrow-forward-circle' size={40} color='#0BE23B'/>
+                </TouchableOpacity>
+              </View>
+            </>
+          </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -46,7 +73,10 @@ const styles = StyleSheet.create({
   },
   footer: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    borderTopWidth: 1,
+    borderTopColor: '#EBEBEB',
+    
     width: '100%',
     padding: 10
   },
@@ -54,7 +84,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     height: 40,
     flex: 1,
-    marginRight: 10,
+    marginRight: 11,
     borderColor: 'transparent',
     backgroundColor: '#ECECEC',
     borderWidth: 1,
